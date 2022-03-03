8d73c24b8856d7c453a1fe9952226fdb5a450325
Danang Arif <darifrahmanda@gmail.com>
2022-03-03 14:44:38 +0700

    update product cat, app css, show cat

--- app/Http/Controllers/ProductCategoryController.php
+++ app/Http/Controllers/ProductCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProductCategoryRequest;
+use App\Models\Product;
 use App\Models\ProductCategory;
 use Illuminate\Http\Request;
 use Yajra\DataTables\Facades\DataTables;
@@ -23,6 +24,8 @@ class ProductCategoryController extends Controller
                 ->addColumn('action', function ($item) {
                     return '<a class="inline-block border border-blue-700 bg-blue-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none
                         hover:bg-blue-800 focus:outline-none focus:shadow-outline" href="' . route('dashboard.category.edit', $item->id) . '">Edit</a>
+                        <a class="inline-block border border-emerald-700 bg-emerald-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none
+                        hover:bg-emerald-800 focus:outline-none focus:shadow-outline" href="' . route('dashboard.category.show', $item->id) . '">Products</a>
                         <form class="inline-block" action="' . route('dashboard.category.destroy', $item->id) . '" method="POST">
                         ' . csrf_field() . method_field('DELETE') . '
                         <button class="inline-block border border-gray-300 bg-gray-300 text-gray-700 rounded-md px-2 py-1 m-1 transition duration-500 ease select-none
@@ -68,7 +71,29 @@ class ProductCategoryController extends Controller
      */
     public function show(ProductCategory $category)
     {
-        //
+        if (request()->ajax()) {
+            $query = Product::with(['category'])->where('categories_id', $category->id)->get();
+
+            return DataTables::of($query)
+                ->addColumn('action', function ($item) {
+                    return '<a class="inline-block border border-blue-700 bg-blue-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none
+                        hover:bg-blue-800 focus:outline-none focus:shadow-outline" href="' . route('dashboard.product.edit', $item->id) . '">Edit</a>
+                        <a class="inline-block border border-green-700 bg-green-700 text-white rounded-md px-2 py-1 m-1 transition duration-500 ease select-none
+                        hover:bg-green-800 focus:outline-none focus:shadow-outline" href="' . route('dashboard.product.gallery.index', $item->id) . '">Gallery</a>
+                        <form class="inline-block" action="' . route('dashboard.product.destroy', $item->id) . '" method="POST">
+                        ' . csrf_field() . method_field('DELETE') . '
+                        <button class="inline-block border border-gray-300 bg-gray-300 text-gray-700 rounded-md px-2 py-1 m-1 transition duration-500 ease select-none
+                        hover:bg-gray-500 hover:text-gray-200 focus:outline-none focus:shadow-outline" type="submit">Delete</button>
+                        </form>
+                        ';
+                })
+                ->editColumn('price', function ($item) {
+                    return 'Rp ' . number_format($item->price, 0, ',', '.');
+                })
+                ->rawColumns(['action'])
+                ->make();
+        }
+        return view('pages.dashboard.category.show', compact('category'));
     }
 
     /**
